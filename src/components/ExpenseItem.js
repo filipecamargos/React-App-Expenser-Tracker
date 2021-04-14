c3e694832ae0a79e0fed41ef98a5fb7efb8dcd84
filipecamargos <filipe.atrabalho@gmail.com>
2021-04-13 18:54:14 -0600

    Setting up the basic dummy data for the component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+  return (
+    <div>
+      <div>April 1st 2025</div>
+      <div>
+        <h2>Car Insurance</h2>
+      </div>
+      <div>$ 200,61</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;

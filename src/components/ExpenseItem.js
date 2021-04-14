c90899b35e35759ea18bfcb53c7a252ece2cb74f
filipecamargos <filipe.atrabalho@gmail.com>
2021-04-14 15:55:54 -0600

    variable added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+
+  const expenseDate = new Date(2021, 4, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 200.61;
+
   return (
     <div className="expense-item">
-      <div>April 1st 2025</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$ 200,61</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );

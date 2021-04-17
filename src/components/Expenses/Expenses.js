c26a3601a27b177fb2e735a0929c5b9f8cf4d66e
filipecamargos <filipe.atrabalho@gmail.com>
2021-04-16 22:44:37 -0600

    Change to harded coded to a dynamic map

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,15 +3,17 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  console.log(props)
   return (
+    props.items.map(expense =>
     <Card className="expenses">
       <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
       ></ExpenseItem>
     </Card>
-  );
+  ));
 }
 
 export default Expenses;

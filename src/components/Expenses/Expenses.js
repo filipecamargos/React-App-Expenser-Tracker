cacf0caee0998cb338c6f4da63182502f7e50e8c
filipecamargos <filipe.atrabalho@gmail.com>
2021-04-19 21:36:00 -0600

    expnseFilterHandeler Added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,34 @@
+import React, {useState} from 'react';
+
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredDate, setFilteredDate] = useState();
+
+  //Update the filtered state
+  const filterHandler = (enteredDate) => {
+    setFilteredDate(enteredDate);
+    console.log(enteredDate);
+  };
+
   return (
-    props.items.map(expense =>
-    <Card className="expenses">
-      <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      ></ExpenseItem>
-    </Card>
-  ));
-}
+    <div>
+      <ExpensesFilter onFilterDateSelected={filterHandler} />
+      {props.items.map((expense) => (
+        <Card className="expenses">
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          ></ExpenseItem>
+        </Card>
+      ))}
+    </div>
+  );
+};
 
 export default Expenses;
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,19 +1,29 @@
+import React, {useState} from 'react';
+
 import "./ExpensesFilter.css";
 
-const ExpensesFilter = () => {
-  return (
-    <div className="expenses-filter">
-      <div className="expenses-filter__control">
-        <label>Filter by year</label>
-        <select>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
-        </select>
-      </div>
-    </div>
-  );
+const ExpensesFilter = (props) => {
+    
+    const [selectedDate, setSelectedDate] = useState('');
+
+    //Handle the date selected
+    const selectedDateHandler = (event) => {
+        setSelectedDate(event.target.value);
+    };
+
+    return (
+        <div className="expenses-filter">
+        <div className="expenses-filter__control">
+            <label>Filter by year</label>
+            <select onChange={selectedDateHandler} defaultValue={setSelectedDate}>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+            <option value="2020">2020</option>
+            <option value="2019">2019</option>
+            </select>
+        </div>
+        </div>
+    );
 };
 
 export default ExpensesFilter;
